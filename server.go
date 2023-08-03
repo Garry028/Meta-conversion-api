e6b51abb462f67b01b7bc91c6bf58e49972e8c72
Gaurav Misal <91176604+Garry028@users.noreply.github.com>
2023-08-03 19:17:45 +0530

    Add files via upload

---
+++ server.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+// Define a struct to represent the destination settings
+type DestinationSettings struct {
+	PixelID      string `json:"pixelId"`
+	AccessKey    string `json:"accessKey"`
+	TestEventCode string `json:"testEventCode"`
+}
+
+// Define a struct to represent the events
+type Event struct {
+	EventName string `json:"eventName"`
+}
+
+// Define a struct to represent the entire payload
+type Payload struct {
+	DestinationSettings DestinationSettings `json:"destinationSettings"`
+	Events              []Event            `json:"events"`
+}
+
+// Function to handle the HTTP POST request for Purchase Events
+func handlePurchaseEvent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Decode the JSON payload into the Payload struct
+	var payload Payload
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the required fields in destination settings
+	if payload.DestinationSettings.PixelID == "" || payload.DestinationSettings.AccessKey == "" || payload.DestinationSettings.TestEventCode == "" {
+		http.Error(w, "Incomplete destination settings data", http.StatusBadRequest)
+		return
+	}
+
+	// Loop through the events and perform any required normalization and hashing
+
+	// Prepare the data to be sent to Facebook
+	data := url.Values{}
+	data.Set("pixelId", payload.DestinationSettings.PixelID)
+	data.Set("accessKey", payload.DestinationSettings.AccessKey)
+	data.Set("testEventCode", payload.DestinationSettings.TestEventCode)
+
+	// Simulate sending the data to the test pixel on Facebook (replace with actual Facebook API call)
+	// For this example, we will just print the data to the console.
+	fmt.Println("Sending data to Facebook: ", data.Encode())
+
+	// Provide a response back to the client (you can customize this based on your requirements)
+	response := map[string]interface{}{
+		"status":  "success",
+		"message": "Purchase events sent to Facebook",
+	}
+	jsonResponse, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
+
+func main() {
+	// Set up a route for handling Purchase Events
+	http.HandleFunc("/purchase-event", handlePurchaseEvent)
+
+	// Start the web server
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+
+/*
+{
+  "destinationSettings": {
+    "pixelId": "dummy_pixel_id",
+    "testEventCode": "dummy_test_event_code",
+    "accessKey": "dummy_access_key"
+  },
+  "events": [
+    {
+      "eventName": "dummy_event_1"
+    },
+    {
+      "eventName": "dummy_event_2"
+    }
+  ]
+}
+
+*/
