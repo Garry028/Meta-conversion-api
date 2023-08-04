ae3830623a22d4422916420e9c5246a6d079783a
Gaurav Misal <91176604+Garry028@users.noreply.github.com>
2023-08-04 11:04:27 +0530

    Update server.go

--- server.go
+++ server.go
@@ -9,8 +9,8 @@ import (
 
 // Define a struct to represent the destination settings
 type DestinationSettings struct {
-	PixelID      string `json:"pixelId"`
-	AccessKey    string `json:"accessKey"`
+	PixelID       string `json:"pixelId"`
+	AccessKey     string `json:"accessKey"`
 	TestEventCode string `json:"testEventCode"`
 }
 
@@ -22,7 +22,7 @@ type Event struct {
 // Define a struct to represent the entire payload
 type Payload struct {
 	DestinationSettings DestinationSettings `json:"destinationSettings"`
-	Events              []Event            `json:"events"`
+	Events              []Event             `json:"events"`
 }
 
 // Function to handle the HTTP POST request for Purchase Events
@@ -47,6 +47,15 @@ func handlePurchaseEvent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Loop through the events and perform any required normalization and hashing
+	for _, event := range payload.Events {
+		// Do normalization and hashing for each event, if needed
+		normalizedEventName := normalizeEventName(event.EventName)
+		hashedEventName := hashEventName(normalizedEventName)
+
+		// Simulate sending the normalized and hashed event data to Facebook
+		// Replace this part with your actual logic to send data to Facebook.
+		fmt.Printf("Sending event to Facebook: %s\n", hashedEventName)
+	}
 
 	// Prepare the data to be sent to Facebook
 	data := url.Values{}
@@ -69,15 +78,26 @@ func handlePurchaseEvent(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
+// Helper function to perform normalization on event names (Replace this with your actual normalization logic)
+func normalizeEventName(eventName string) string {
+	// Placeholder normalization logic, you should implement your own
+	return eventName
+}
+
+// Helper function to perform hashing on event names (Replace this with your actual hashing logic)
+func hashEventName(eventName string) string {
+	// Placeholder hashing logic, you should implement your own
+	return eventName
+}
+
 func main() {
 	// Set up a route for handling Purchase Events
-	http.HandleFunc("/purchase-event", handlePurchaseEvent)
+	http.HandleFunc("/send/facebook", handlePurchaseEvent)
 
 	// Start the web server
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
-
 
 /*
 {
